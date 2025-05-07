eea73295e5a1d6c17431e2cca2f1c540548b6e7a
PumbaaTheWarthog <jonathanpatricksvaerke.edu@gmail.com>
2025-05-07 19:15:34 +0200

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "My NixOS Flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+  };
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      desktop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/desktop # System settings
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.jonathan = import ./home/jonathan/home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
