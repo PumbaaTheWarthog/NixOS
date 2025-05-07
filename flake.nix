da9bef014f670a3ede336fbff69aae2ef660c906
PumbaaTheWarthog <jonathanpatricksvaerke.edu@gmail.com>
2025-05-07 19:44:19 +0200

    Update flake.nix
    Create laptop option to flake.nix

--- flake.nix
+++ flake.nix
@@ -19,6 +19,17 @@
           }
         ];
       };
+      laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/laptop # System settings
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.jonathan = import ./home/jonathan/home.nix;
+          }
+        ];
+      };
     };
   };
 }
