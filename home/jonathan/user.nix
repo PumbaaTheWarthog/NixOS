fea20da2c1fba0f9b9488f58c98f81dc52063a85
PumbaaTheWarthog <jonathanpatricksvaerke.edu@gmail.com>
2025-05-08 08:21:32 +0200

    Update user.nix
    Gnome

--- home/jonathan/user.nix
+++ home/jonathan/user.nix
@@ -3,4 +3,10 @@
     isNormalUser  = true;
     home  = "/home/jonathan";
   };
+
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
 }
