0c0211d5a05145215152c2e30a44593086499fa3
PumbaaTheWarthog <jonathanpatricksvaerke.edu@gmail.com>
2025-05-14 13:44:09 +0200

    Update vscode.nix

--- home/jonathan/programs/vscode.nix
+++ home/jonathan/programs/vscode.nix
@@ -1,3 +1,26 @@
 { config, pkgs, ... }: {
-
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscode; # Can be changed to vscodium
+    profiles = {
+      latex = {
+        extensions = with pkgs.vscode-extensions; [
+          james-yu.latex-workshop
+        ];
+        userSettings = {
+          # Define user settings
+        };
+      };
+      python = {
+        extensions = with pkgs.vscode-extensions; [
+          ms-python.python
+          ms-python.vscode-pylance
+          ms-python.debugpy
+        ];
+        userSettings = {
+          # Define user settings
+        };
+      };
+    };
+  };
 }
