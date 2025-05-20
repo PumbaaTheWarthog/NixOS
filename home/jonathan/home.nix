74ab6539aaed191b72f067079cbc8396de12eeb9
jonathansvaerke <jonathansvaerke@proton.me>
2025-05-20 12:37:12 +0200

    Update home.nix

--- home/jonathan/home.nix
+++ home/jonathan/home.nix
@@ -4,7 +4,7 @@
   home = {
     username = "jonathan";
     homeDirectory = "/home/jonathan";
-    stateVersion = "24.11";
+    stateVersion = "25.05";
     packages = with pkgs; [
       firefox
       vscode
