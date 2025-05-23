6e57012df574e833a4d6d34e4f842a7a2b2d2de6
PumbaaTheWarthog <jonathanpatricksvaerke.edu@gmail.com>
2025-05-23 12:43:23 +0200

    Remove nesting from vscode config ;(

--- home/jonathan/programs/vscode.nix
+++ home/jonathan/programs/vscode.nix
@@ -8,102 +8,85 @@
           james-yu.latex-workshop
         ];
         userSettings = {
-          "editor" = {
-            "acceptSuggestionOnCommitCharacter" = false;
-            "acceptSuggestionOnEnter" = "on";
-            "accessibilitySupport" = "off";
-            "autoClosingBrackets" = "always";
-            "autoClosingQuotes" = "always";
-            "autoIndent" = "full";
-            "bracketPairColorization.enabled" = true;
-            "fontSize" = 16;
-            "formatOnSave" = true;
-            "suggestOnTriggerCharacters" = true;
-            "tabSize" = 4;
-            "wordWrap" = "off";
-          };
-        
-          "latex-workshop" = {
-            "bind.enter.key" = false;
-            "formatting" = {
-              "latex" = "latexindent";
-            };
-            "hover" = {
-                "citation.enable" = true;
-                "command.enable" = true;
-            };
-            "intellisense" = {
-              "citation.backend" = "biblatex";
-              "package.enabled" = true;
-              "package.extra" = [
-                "amsmath"
-                "mhchem"
-                "chemfig"
-              ];
-              };
-              "latex" = {
-                "autoBuild.run" = "onSave";
-                "autoClean.run" = "onBuilt";
-                "clean.fileTypes" = [ "*.aux" "*.bbl" "*.blg" "*.idx" "*.ind" "*.lof" "*.lot" "*.out" "*.toc" "*.acn" "*.acr" "*.alg" "*.glg" "*.glo" "*.gls" "*.fls" "*.log" "*.fdb_latexmk" "*.snm" "*.synctex(busy)" "*.synctex.gz(busy)" "*.nav" "*.vrb" "*.xdv" ];
-                "recipes" = [
-                  {
-                    "name" = "latexmk";
-                    "tools" = [ "latexmk" ];
-                  }
-                  {
-                    "name" = "latexmk -> bibtex -> latexmk * 2";
-                    "tools" = [ "latexmk" "bibtex" "latexmk" "latexmk" ];
-                  }
-                  {
-                    "name" = "pdflatex";
-                    "tools" = [ "pdflatex" ];
-                  }
-                  {
-                    "name" = "pdflatex -> bibtex -> pdflatex * 2";
-                    "tools" = [ "pdflatex" "bibtex" "pdflatex" "pdflatex" ];
-                  }
-                ];
-                "tools" = [
-                  {
-                    "name" = "latexmk";
-                    "command" = "latexmk";
-                    "args" = [ "-synctex=1" "-interaction=nonstopmode" "-file-line-error" "-pdf" "-outdir=%OUTDIR%" "%DOC%" ];
-                    "env" = {};
-                  }
-                  {
-                    "name" = "pdflatex";
-                    "command" = "pdflatex";
-                    "args" = [ "-synctex=1" "-interaction=nonstopmode" "-file-line-error" "-outdir=%OUTDIR%" "%DOC%" ];
-                    "env" = {};
-                  }
-                  {
-                    "name" = "bibtex";
-                    "command" = "bibtex";
-                    "args" = ["%DOCFILE%"];
-                  }
-                  {
-                    "name" = "biber";
-                    "command" = "biber";
-                    "args" = ["%DOCFILE%"];
-                  }
-                ];
-              };
-              "message" = {
-                "error.show" = false;
-                "warning.show" = false;
-              };
-              "view" = {
-                "pdf.dark.backgroundColor" = "#1e1e1e";
-                "pdf.zoom" = "page-fit";
-              };
-            };
-        
-          "workbench" = {
-            "colorTheme" = "Default Dark Modern";
-            "preferredDarkColorTheme" = "Default Dark Modern";
-            "sideBar.location" = "right";
-            "statusBar.visible" = true;
-          };
+          "editor.acceptSuggestionOnCommitCharacter" = false;
+          "editor.acceptSuggestionOnEnter" = "on";
+          "editor.accessibilitySupport" = "off";
+          "editor.autoClosingBrackets" = "always";
+          "editor.autoClosingQuotes" = "always";
+          "aeditor.utoIndent" = "full";
+          "editor.bracketPairColorization.enabled" = true;
+          "editor.fontSize" = 16;
+          "editor.formatOnSave" = true;
+          "editor.suggestOnTriggerCharacters" = true;
+          "editor.tabSize" = 4;
+          "editor.wordWrap" = "off";
+
+          "latex-workshop.bind.enter.key" = false;
+          "latex-workshop.formatting.latex" = "latexindent";
+          "latex-workshop.hover.citation.enable" = true;
+          "latex-workshop.hover.command.enable" = true;
+          "latex-workshop.intellisense.citation.backend" = "biblatex";
+          "latex-workshop.intellisense.package.enabled" = true;
+          "latex-workshop.intellisense.package.extra" = [
+            "amsmath"
+            "mhchem"
+            "chemfig"
+          ];
+
+          "latex-workshop.latex.autoBuild.run" = "onSave";
+          "latex-workshop.latex.autoClean.run" = "onBuilt";
+          "latex-workshop.latex.clean.fileTypes" = [ "*.aux" "*.bbl" "*.blg" "*.idx" "*.ind" "*.lof" "*.lot" "*.out" "*.toc" "*.acn" "*.acr" "*.alg" "*.glg" "*.glo" "*.gls" "*.fls" "*.log" "*.fdb_latexmk" "*.snm" "*.synctex(busy)" "*.synctex.gz(busy)" "*.nav" "*.vrb" "*.xdv" ];
+          "latex-workshop.latex.recipes" = [
+            {
+              "name" = "latexmk";
+              "tools" = [ "latexmk" ];
+            }
+            {
+              "name" = "latexmk -> bibtex -> latexmk * 2";
+              "tools" = [ "latexmk" "bibtex" "latexmk" "latexmk" ];
+            }
+            {
+              "name" = "pdflatex";
+              "tools" = [ "pdflatex" ];
+            }
+            {
+              "name" = "pdflatex -> bibtex -> pdflatex * 2";
+              "tools" = [ "pdflatex" "bibtex" "pdflatex" "pdflatex" ];
+            }
+          ];
+          "latex-workshop.latex.tools" = [
+            {
+              "name" = "latexmk";
+              "command" = "latexmk";
+              "args" = [ "-synctex=1" "-interaction=nonstopmode" "-file-line-error" "-pdf" "-outdir=%OUTDIR%" "%DOC%" ];
+              "env" = {};
+            }
+            {
+              "name" = "pdflatex";
+              "command" = "pdflatex";
+              "args" = [ "-synctex=1" "-interaction=nonstopmode" "-file-line-error" "-outdir=%OUTDIR%" "%DOC%" ];
+              "env" = {};
+            }
+            {
+              "name" = "bibtex";
+              "command" = "bibtex";
+              "args" = ["%DOCFILE%"];
+            }
+            {
+              "name" = "biber";
+              "command" = "biber";
+              "args" = ["%DOCFILE%"];
+            }
+          ];
+          "latex-workshop.message..error.show" = false;
+          "latex-workshop.message..warning.show" = false;
+          "latex-workshop.view.pdf.dark.backgroundColor" = "#1e1e1e";
+          "latex-workshop.view.pdf.zoom" = "page-fit";
+
+          "workbench.colorTheme" = "Default Dark Modern";
+          "workbench.preferredDarkColorTheme" = "Default Dark Modern";
+          "workbench.sideBar.location" = "right";
+          "workbench.statusBar.visible" = true;
         };
       };
       python = {
