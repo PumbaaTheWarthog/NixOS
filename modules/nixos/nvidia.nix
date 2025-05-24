1a2e6e51f8f01b3ce127b8c9f7639f3b61948b5f
jonathansvaerke <jonathansvaerke@proton.me>
2025-05-24 11:27:52 +0200

    Update nvidia.nix

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -3,6 +3,8 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
     open = true; # Whether to enable the open source NVIDIA kernel module.
     modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
     nvidiaSettings = true; # Whether to enable nvidia-settings, NVIDIA’s GUI configuration tool.
   };
   services.xserver.videoDrivers = [ "nvidia" ];
